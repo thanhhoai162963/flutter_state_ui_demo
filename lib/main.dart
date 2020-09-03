334330b313db2abea1b4e2ef7fabb1d155772076
thanhhoai162963 <hoaithanh249@gmail.com>
2020-09-03 15:43:51 +0700

    statefull and stateless

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -23,41 +27,56 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(backgroundColor: Colors.deepOrangeAccent,
-        title: Text("login"),
+      appBar: AppBar(
+        title: Text("Login"),
       ),
-      body: Container(
+      body: Center(
+        child: Container(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                icon: Icon(Icons.email),
-                hintText: "email",
-                labelText: "email",
-              ),
-            ),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                  icon: Icon(Icons.person),
-                  hintText: "user",
-                  labelText: "user"
-              ),
-            ),
-            RaisedButton(
-              child: Text("login", style: TextStyle(color: Colors.white)),
-              color: Colors.lightBlueAccent,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              onPressed: () {},
-            )
+            Text("Home Page"),
+            Countpage(),
           ],
         ),
+      ),
       ),
     );
   }
 }
+class Countpage extends StatefulWidget {
+  @override
+  _CountpageState createState() => _CountpageState();
+}
+
+class _CountpageState extends State<Countpage> {
+  var count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Text("data $count",style: TextStyle(fontSize: 20),),
+          SizedBox(height: 10,),
+          RaisedButton(
+            color: Colors.blue,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+            child: Text("Click me",style: TextStyle(color: Colors.white)),
+            onPressed: (){
+              setState(() {
+                count++;
+              });
+            },
+          )
+        ],
+      )
+    );
+  }
+}
+
+
+
+
+
 
 
